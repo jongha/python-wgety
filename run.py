d00051f4588d118e25c1db35eae0287bbf9fa50c
Jong-Ha Ahn <onpissoft@gmail.com>
2013-12-03 15:25:26 +0900

    add exit code

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 from wgety.wgety import Wgety
 
-w = Wgety()
-w.execute(url='http://www.python.org', filename='python.html', absolute_link=True);
-w.execute(url='http://www.python.org/images/python-logo.gif');
+try:
+    w = Wgety()
+    w.execute(url='http://www.python.org', filename='python.html', absolute_link=True);
+    w.execute(url='http://www.python.org/images/python-logo.gif');
+    exit(1)
+    
+except:
+    exit(0)
